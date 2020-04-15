4abd77d4a8a772a18ce41bc6cb7b68761ae440e0
Rav52ant <Rav52ant@DESKTOP-GUGU8QH>
2020-04-15 23:12:08 +0530

    m5 method is Added

--- EmployeeProj/src/com/app/emp/f2.java
+++ EmployeeProj/src/com/app/emp/f2.java
@@ -1,10 +1,15 @@
 package com.app.emp;
 
 public class f2 {
-public void f2() {
-	System.out.println("f2.f2()");
-}
-public void f3() {
-	System.out.println("f2.f3()");
-}
+	public void f2() {
+		System.out.println("f2.f2()");
+	}
+
+	public void f3() {
+		System.out.println("f2.f3()");
+	}
+
+	public void m5() {
+		System.out.println("f2.m5()");
+	}
 }
